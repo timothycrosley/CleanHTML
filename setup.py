ae1ec6929852196aa515191d17b7663ea9e2d246
Timothy Crosley <timothy.crosley@gmail.com>
2013-01-18 16:19:12 -0500

    CleanHTML-0.2.0: Add GPLV2 docs, add support for Python 3 and WebElements 1.0.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='CleanHTML',
-      version='0.1',
+      version='0.2.0',
       description='A Very Forgiving HTML/XML Cleaner.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/CleanHTML/blob/master/dist/CleanHTML-0.1.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/CleanHTML/blob/master/dist/CleanHTML-0.2.0.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/cleanHTML',],
       requires = ['webelements',],)
